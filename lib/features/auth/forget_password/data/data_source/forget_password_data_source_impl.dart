160ed962cf4b4c1368bc3e2d038908649fe40a2a
Mariam2572 <mariamkamel791@gmail.com>
2025-05-04 15:15:51 +0300

    feat: implement forget password feature with data sources, repository, cubit, and state management

---
+++ lib/features/auth/forget_password/data/data_source/forget_password_data_source_impl.dart
@@ -0,0 +1,3 @@
+import 'package:tracking_app/features/auth/forget_password/domain/data_source/forget_password_data_source.dart';
+
+class ForgetPasswordDataSourceImpl implements ForgetPasswordDataSource {}
---
+++ lib/features/auth/forget_password/data/repo/forget_password_repo_impl.dart
@@ -0,0 +1,3 @@
+import 'package:tracking_app/features/auth/forget_password/domain/repo/forget_password_repo.dart';
+
+class ForgetPasswordRepoImpl implements ForgetPasswordRepo {}
