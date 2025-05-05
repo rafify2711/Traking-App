4141c8a95e82529caa12bde1ebe93eab1cc9ace5
FatmaNasser29 <fatma.nasser1213@gmail.com>
2025-05-05 21:04:35 +0300

    implement onBoarding logic

--- lib/features/splash/presentation/views/widgets/on_boarding_view.dart
+++ lib/features/splash/presentation/views/widgets/on_boarding_view.dart
@@ -1,13 +1,68 @@
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:tracking_app/generated/local_keys.g.dart';
+import 'package:lottie/lottie.dart';
+import 'package:tracking_app/core/utils/app_text_styles.dart';
 
-class OnBoardingView extends StatelessWidget {
+class OnBoardingView extends StatefulWidget {
   const OnBoardingView({super.key});
+
+  @override
+  State<OnBoardingView> createState() => _OnBoardingViewState();
+}
+
+class _OnBoardingViewState extends State<OnBoardingView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text(LocaleKeys.WelcomeToFloweryRiderApp.tr())),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(right: 16, left: 16, top: 8),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(
+                  height: 444,
+                  width: double.infinity,
+                  child: Container(
+                    child: Lottie.asset(
+                      'assets/json/on_boarding.json',
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 16),
+                Text(
+                  "Welcome to \nFlowery rider app",
+                  style: AppTextStyles.instance.textStyle20.copyWith(
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(height: 24),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Login",
+                    style: AppTextStyles.instance.textStyle16.copyWith(
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                OutlinedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Apply now",
+                    style: AppTextStyles.instance.textStyle16.copyWith(
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
