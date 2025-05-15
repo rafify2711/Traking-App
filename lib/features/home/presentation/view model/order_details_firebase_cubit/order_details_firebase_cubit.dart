091376c96ddb7ba4ca62fe80af3c9c9ae0ecdd5c
Hekmat <m.hekmatfanari@gmail.com>
2025-05-16 00:42:56 +0300

    add unit testing for fetch order details from firebase and remove print from classes

--- lib/features/home/presentation/view model/order_details_firebase_cubit/order_details_firebase_cubit.dart
+++ lib/features/home/presentation/view model/order_details_firebase_cubit/order_details_firebase_cubit.dart
@@ -1,40 +1,34 @@
 import 'package:bloc/bloc.dart';
-import 'package:easy_localization/easy_localization.dart';
 import 'package:equatable/equatable.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:tracking_app/core/base/base_state.dart';
 import 'package:tracking_app/features/home/data/models/order_details.dart';
-import 'package:tracking_app/features/home/domain/use_case/get_all_pending_orders_use_case.dart';
 import 'package:tracking_app/features/home/domain/use_case/get_orderdetails_from_firebase.dart';
-import 'package:tracking_app/generated/locale_keys.g.dart';
 
 part 'order_details_firebase_state.dart';
 
 class OrderDetailsFirebaseCubit extends Cubit<OrderDetailsFirebaseState> {
   final GetOrderdetailsFromFirebase orderUseCase;
 
-  OrderDetailsFirebaseCubit( this.orderUseCase)
+  OrderDetailsFirebaseCubit(this.orderUseCase)
     : super(const OrderDetailsFirebaseState());
 
   Future<void> fetchOrderDetails() async {
     emit(state.copyWith(orderState: BaseLoading<OrderDetails>()));
 
-    try{
+    try {
       final result = await orderUseCase.invoke();
-print(result.totalPrice);
-print("aaaaaaaa");
-      emit(state.copyWith(orderState: BaseSuccess<OrderDetails>(data: result),order: result));
-
-    }catch(e){
-      print(e);
-      print("eeeeeeeeeeeee");
+      emit(
+        state.copyWith(
+          orderState: BaseSuccess<OrderDetails>(data: result),
+          order: result,
+        ),
+      );
+    } catch (e) {
       emit(
         state.copyWith(
           orderState: BaseError<OrderDetails>(errorMessage: e.toString()),
         ),
       );
-
-
     }
   }
 }
