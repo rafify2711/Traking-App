a1af34d40e65968e212bda370f91eb34ee785590
Mariam2572 <mariamkamel791@gmail.com>
2025-06-04 21:29:18 +0300

    refactor: Update uploadPhotoUseCase and editProfile method calls for consistency

--- lib/features/editProfile/presentation/viewModel/cubit/edit_profile_cubit.dart
+++ lib/features/editProfile/presentation/viewModel/cubit/edit_profile_cubit.dart
@@ -4,14 +4,17 @@ import 'package:easy_localization/easy_localization.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
+import 'package:tracking_app/core/base/api_result.dart';
+import 'package:tracking_app/features/editProfile/data/model/user_response/user_response.dart';
 import 'package:tracking_app/features/editProfile/domain/useCase/edit_profile_use_case.dart';
+import 'package:tracking_app/features/editProfile/domain/useCase/upload_photo.dart';
 import 'package:tracking_app/features/editProfile/presentation/viewModel/cubit/edit_profile_state.dart';
 import 'package:tracking_app/generated/locale_keys.g.dart';
 
 @injectable
 class EditProfileCubit extends Cubit<EditProfileState> {
   final EditProfileUseCase editProfileUseCase;
-  final UploadPhotoUseCase uploadPhotoUseCase;
+  final UploadPhoto uploadPhotoUseCase;
 
   EditProfileCubit(this.editProfileUseCase, this.uploadPhotoUseCase)
     : super(const EditProfileState());
@@ -20,7 +23,7 @@ class EditProfileCubit extends Cubit<EditProfileState> {
     try {
       emit(state.copyWith(status: EditProfileStatus.loading));
 
-      await uploadPhotoUseCase(photo!);
+      await uploadPhotoUseCase.uploadPhoto(photo!);
 
       emit(state.copyWith(status: EditProfileStatus.success));
     } catch (e) {
@@ -36,18 +39,14 @@ class EditProfileCubit extends Cubit<EditProfileState> {
   Future<void> editProfile(UpdatedUserModel updatedUser) async {
     emit(state.copyWith(status: EditProfileStatus.loading));
 
-    final result = await editProfileUseCase(updatedUser);
+    final result = await editProfileUseCase.editProfile(updatedUser);
 
-    result.fold(
-      (failure) => emit(
-        state.copyWith(
-          status: EditProfileStatus.failure,
-          errorMessage: failure.errorMessage,
-        ),
-      ),
-      (profile) => emit(
-        state.copyWith(status: EditProfileStatus.success, profile: profile),
-      ),
-    );
+    if (result is ApiSuccess<UserResponse>) {
+      emit(state.copyWith(status: EditProfileStatus.success, profile: result.data));
+    } else if (result is ApiError<UserResponse>) {
+      emit(state.copyWith(status: EditProfileStatus.failure, errorMessage: result.failure?.errorMessage ?? 'Unknown error'));
+    }
+  
+    
   }
 }
